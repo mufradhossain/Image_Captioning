88d79e68b821b6eac8bfbcaa910c51634ec53e2d
mufradhossain <sheikh15-2773@diu.edu.bd>
2021-03-06 17:16:10 +0600

    Files Added

---
+++ Image_Caption_St.py
@@ -0,0 +1,125 @@
+from PIL import Image
+import streamlit as st
+from keras.models import load_model
+import numpy as np
+from keras.layers import Dense, LSTM, TimeDistributed, Embedding,  Activation, RepeatVector,Concatenate
+from keras.models import Sequential, Model
+import cv2
+from keras.preprocessing.sequence import pad_sequences
+from tqdm import tqdm
+
+
+vocab = np.load('vocab.npy', allow_pickle=True)
+
+vocab = vocab.item()
+
+inv_vocab = {v:k for k,v in vocab.items()}
+
+embedding_size = 128
+vocab_size = len(vocab)
+max_len = 40
+
+image_model = Sequential()
+
+image_model.add(Dense(embedding_size, input_shape=(2048,), activation='relu'))
+image_model.add(RepeatVector(max_len))
+
+language_model = Sequential()
+
+language_model.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, input_length=max_len))
+language_model.add(LSTM(256, return_sequences=True))
+language_model.add(TimeDistributed(Dense(embedding_size)))
+
+conca = Concatenate()([image_model.output, language_model.output])
+x = LSTM(128, return_sequences=True)(conca)
+x = LSTM(512, return_sequences=False)(x)
+x = Dense(vocab_size)(x)
+out = Activation('softmax')(x)
+model = Model(inputs=[image_model.input, language_model.input], outputs = out)
+
+model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
+
+model.load_weights('mine_model_weights.h5')
+
+resnet = load_model('resnet.h5')
+
+
+
+def after(our_image):
+    global model, resnet, vocab, inv_vocab
+
+    image=our_image
+
+    image = cv2.cvtColor(np.float32(image), cv2.COLOR_BGR2RGB)
+
+    image = cv2.resize(image, (224, 224))
+
+    image = np.reshape(image, (1, 224, 224, 3))
+
+    incept = resnet.predict(image).reshape(1, 2048)
+
+
+
+    text_in = ['startofseq']
+
+    final = ''
+
+
+
+    count = 0
+    while tqdm(count < 20):
+
+        count += 1
+
+        encoded = []
+        for i in text_in:
+            encoded.append(vocab[i])
+
+        padded = pad_sequences([encoded], maxlen=max_len, padding='post', truncating='post').reshape(1, max_len)
+
+        sampled_index = np.argmax(model.predict([incept, padded]))
+
+        sampled_word = inv_vocab[sampled_index]
+
+        if sampled_word != 'endofseq':
+            final = final + ' ' + sampled_word
+
+        text_in.append(sampled_word)
+
+        text_in_str = ' '.join([str(elem) for elem in text_in]) 
+        
+        head, sep, tail = text_in_str.partition('.')
+
+        cap = head[10:]
+
+    return cap
+
+
+def main():
+    """Image Captioning App"""
+
+    st.title("Image Caption")
+
+    html_temp = """
+    <body style="background-color:red;">
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Image Captioning WebApp</h2>
+    </div>
+    </body>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    image_file = st.file_uploader("Upload Image", type=['jpg', 'png', 'jpeg'])
+    if image_file is not None:
+        our_image = Image.open(image_file)
+
+        st.image(our_image)
+
+    if st.button("Recognise"):
+       res= after(our_image)
+       res2=str(res)
+       st.text("Caption: " + res2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run Image_Caption_St.py
